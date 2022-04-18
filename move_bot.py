427edf9496f7df4f8a8f207a0a3558190f8e0e2c
drEEm tEEm <mmuldown@caltech.edu>
2022-04-18 16:15:07 -0700

    problem 7

--- move_bot.py
+++ move_bot.py
@@ -2,26 +2,30 @@
 import motor
 import time
 
+def drive_shape(turns):
+    for i in range(turns):
+        robo.setlinear(0.5)
+        time.sleep(1)
+        robo.setspin(360/turns)
+
+def straight_and_back():
+    drive_shape(2)
+
+def triangle():
+    drive_shape(3)
+
+def square():
+    drive_shape(4)
+
 if __name__ == "__main__":
     #initialize the robot
     robo = motor.Motor()
 
     try:
-        # Moving robot linearly
-#         robo.setlinear(0.16)
-#         time.sleep(2)
-        # Testing spinning
-        # robo.set(0.6, -0.6)
-        #time.sleep(1)
-        robo.setlinear(0.5)
-        time.sleep(1)
-        robo.setspin(180)
-        robo.setlinear(0.5)
-        time.sleep(1)
-        robo.setspin(180)
-        
+        triangle()
     except BaseException as ex:
         print("Ending due to exception: %s" % repr(ex))
         
     #shut down robot
     robo.shutdown()
+

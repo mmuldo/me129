19271963e206a66e96f3e94043606f4912ec48a2
drEEm tEEm <mmuldown@caltech.edu>
2022-04-24 15:53:16 -0700

    NEW ONE-STOP SHOP FOR EEBOT STUFF ports over some motor stuff from motor.py implements initialization of LEDS

---
+++ robot.py
@@ -0,0 +1,180 @@
+import pigpio
+import sys
+import math
+
+from typing import List, Tuple
+
+# first element is the left motor; second is the right.
+# first element of each tuple is the negative terminal; second is positive
+MOTOR_PINS = [(7, 8), (5, 6)]
+
+# colors correspond to the color of the jumper cable connected to each LED
+LED_PINS = {
+    18: 'orange',
+    15: 'brown',
+    14: 'white',
+}
+
+def duty_to_pwm(
+    motor_number: int,
+    dutycycle: float
+) -> Tuple[Tuple[int, int], Tuple[int, int]]:
+    '''
+    converts dutycycle to pwm
+
+    Parameters
+    ----------
+    motor_number : int
+        0 -> for left motor, 1 -> for right motor; errors if anything else
+    dutycycle : float
+        dutycycle, between -1.0 and 1.0
+
+    Returns
+    -------
+    Tuple[Tuple[int, int], Tuple[int, int]]
+        two tuples: the first is the (motor_pin, pwm) for the pin
+        of the motor being set and the second is (other_motor_pin, 0)
+    '''
+    magnitude = abs(round(dutycycle*255))
+    if magnitude > 255:
+        # max pwm is 255
+        magnitude = 255
+
+    # negative dutycycle -> negative pin, positive -> positive pin
+    pin_to_set = 0 if dutycycle < 0 else 1
+    other_pin = 0 if pin_to_set == 1 else 1
+
+    return ((MOTOR_PINS[motor_number][pin_to_set], magnitude),
+            (MOTOR_PINS[motor_number][other_pin], 0))
+
+class EEBot:
+    '''eebot instance!
+
+    Attributes
+    ----------
+    io : pigpio.pi
+        pi I/O instance with which we can interact with our little eebot
+    motors: List[int]
+        GPIO pins that the motor leads are connected to;
+        looking at the bot from the back, the pin connections in order
+        are expected to be: [LEFT_NEG, LEFT_POS, RIGHT_NEG, RIGHT_POS]
+    LED_detectors : List[int]
+        GPIO pins that the LED/phototransistor detectors are connected to
+    L : float
+        distance from the rear axle to the caster wheel in meters
+    d : float
+        distance between the two wheels in meters
+
+    Methods
+    -------
+    shutdown()
+        stops robot
+    detectors_status()
+        prints the status of each LED detector
+    set_pwm(leftdutycycle, rightdutycycle)
+        sets pwm of left and right motors
+    '''
+
+    def __init__(
+        self,
+        motors: List[int],
+        LED_detectors: List[int],
+        L: float = 0.103,
+        d: float = 0.131
+    ):
+        '''initializes eebot
+
+        Parameters
+        ----------
+        motors : List[int]
+            GPIO
+        LED_detectors : List[int]
+            GPIO pins that the LED/phototransistor detectors are connected to
+        '''
+        self.motors = motors
+        self.LED_detectors = LED_detectors
+        self.L = L
+        self.d = d
+
+        self.io = pigpio.pi()
+        if not self.io.connected:
+            print("Unable to connection to pigpio daemon!")
+            sys.exit(0)
+
+        for pin in motors:
+            # Set up the four pins as output (commanding the motors).
+            self.io.set_mode(pin, pigpio.OUTPUT)
+
+            # Prepare the PWM.  The range gives the maximum value for 100%
+            # duty cycle, using integer commands (1 up to max).
+            self.io.set_PWM_range(pin, 255)
+
+            # Set the PWM frequency to 1000Hz.  You could try 500Hz or 2000Hz
+            # to see whether there is a difference?
+            self.io.set_PWM_frequency(pin, 1000)
+
+            # Clear all pins, just in case.
+            self.io.set_PWM_dutycycle(pin, 0)
+
+        for pin in LED_detectors:
+            # setup four LED pins as input
+            self.io.set_mode(pin, pigpio.INPUT)
+
+    def shutdown(self):
+        '''stops robot'''
+        print("Turning off...")
+
+        for pin in self.motors:
+            self.io.set_PWM_dutycycle(pin, 0)
+
+        self.io.stop()
+
+
+    def detectors_status(self):
+        '''prints status of each LED detector'''
+        for pin in self.LED_detectors:
+            print(LED_PINS[pin], self.io.read(pin))
+
+    def set_pwm(self, leftdutycycle: float, rightdutycycle: float):
+        '''
+        sets the PWM for both motors
+
+        Parameters
+        ----------
+        leftdutycycle
+            dutycycle for left motor, between -1.0 and 1.0
+        rightdutycycle
+            dutycycle for left motor, between -1.0 and 1.0
+        '''
+        for motor_number, dutycycle in enumerate([leftdutycycle, rightdutycycle]):
+            motor_pwm = duty_to_pwm(motor_number, dutycycle)
+
+            self.io.set_PWM_dutycycle(*motor_pwm[0])
+            self.io.set_PWM_dutycycle(*motor_pwm[1])
+
+    def set(self, vel_nominal: float, steering_angle: float):
+        '''
+        sets the nominal velocity and steering angle
+
+        Parameters
+        ----------
+        vel_nominal : float
+            nominal velocity in m/s
+        steering_angle : float
+            steering angle in degrees
+        '''
+        theta = steering_angle * math.pi / 180
+
+        vel_left = vel_nominal * (math.cos(theta) -
+                                  self.d/self.L * math.sin(theta))
+        vel_right = vel_nominal * (math.cos(theta) +
+                                   self.d/self.L * math.sin(theta))
+
+        # TODO: FIX ME!!!!!
+        k = 1.5
+        fric = .34 if vel_nominal > 0 else -.34
+
+        self.set_pwm(
+            k*vel_left + fric,
+            k*vel_right + fric
+        )
